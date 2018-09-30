13b8d6eff73ef43c6d1d78aa3507ac18418d7cae
Vasiliy Kashirin <vasich61@MacBook-Pro-Vasiliy.local>
2018-09-30 22:48:03 +0300

    created clion project v1

---
+++ cmake-build-debug/CMakeFiles/theproject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/theproject.dir/main.c.o"
+  "theproject.pdb"
+  "theproject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/theproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
